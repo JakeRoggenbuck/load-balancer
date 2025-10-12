da29c2ec14303dd4b3edee432f3bff917468ac0b
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-10-12 13:32:20 -0700

    Add Handler

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"log"
+	"net/http"
 	"os"
 )
+
+var POOL Pool
 
 type Application struct {
 	Alive bool
@@ -27,22 +30,30 @@ func (p *Pool) get_application() Application {
 	return app
 }
 
+func universalHandler(w http.ResponseWriter, r *http.Request) {
+	// This will receive any request to any path
+	// I can then call my own requests to applications
+	fmt.Printf("Received request: %s %s\n", r.Method, r.URL.Path)
+
+	app := POOL.get_application()
+	fmt.Println(app)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Handled: %s", r.URL.Path)
+}
+
 func main() {
 	content, err := os.ReadFile("applications.toml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var pool Pool
-	if _, err := toml.Decode(string(content), &pool); err != nil {
+	if _, err := toml.Decode(string(content), &POOL); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(pool)
+	http.HandleFunc("/", universalHandler)
 
-	for i := range 10 {
-		app := pool.get_application()
-
-		fmt.Println(i, app)
-	}
+	fmt.Println("Load balancer listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
